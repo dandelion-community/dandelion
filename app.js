3ab56cd02b08b2f56d72d3ee4eab18846a61fd11
Lowell Bander <lowellbander@gmail.com>
2021-12-07 20:23:44 +0000

    validating that new endpoint deploys to aws

--- app.js
+++ app.js
@@ -15,20 +15,24 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.get('/mongo-dev', (req, res) => {
+  res.send('this is where we will show mongo results');
+});
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
