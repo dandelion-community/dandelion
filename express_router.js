33dbea5c2795ec802be4fbcda6923277d20f6b59
Chris Barker <bluquar@gmail.com>
2021-12-08 13:20:43 +0000

    re-add error view

--- express_router.js
+++ express_router.js
@@ -6,10 +6,15 @@ var logger = require('morgan');
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/mongo-dev', (req, res) => {
   res.send('this is where we will show mongo results');
@@ -30,7 +35,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.send('error');
+  res.render('error');
 });
 
 module.exports = app;
