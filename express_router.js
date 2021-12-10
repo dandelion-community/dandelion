b5a1239a3c78b3cba84d7def395d1112d7978f23
Chris Barker <bluquar@gmail.com>
2021-12-10 07:43:38 -0800

    Bring in boilerplate expo app (#6)

--- express_router.js
+++ express_router.js
@@ -12,7 +12,7 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.get('/mongo-dev', (req, res) => {
