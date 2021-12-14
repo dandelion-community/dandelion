51ee09d6262e75eaadc78ad6fa360a3b3efa3eaa
Chris Barker <bluquar@gmail.com>
2021-12-14 08:35:25 -0800

    Implement login/logout/register/me in GraphQL (#30)
    * Implement login/logout/register/me in GraphQL
    
    * Implement login/logout/register/me in GraphQL

---
+++ src/server/graphql/schema.ts
@@ -0,0 +1,52 @@
+import type { Express } from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
+import {
+  login,
+  logout,
+  me,
+  register,
+  USER_GQL,
+  CURRENT_USER_PAYLOAD_GQL,
+} from '../authentication/graphql';
+
+const schema = buildSchema(`
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(
+      username: String!
+      password: String!
+    ): CurrentUserPayload
+
+    logout: CurrentUserPayload
+
+    register(
+      username: String!
+      password: String!
+    ): CurrentUserPayload
+  }
+
+  ${USER_GQL}
+  ${CURRENT_USER_PAYLOAD_GQL}
+`);
+
+const rootValue = {
+  login,
+  logout,
+  me,
+  register,
+};
+
+export function initGraphQL(app: Express): void {
+  app.use(
+    '/graphql',
+    graphqlHTTP({
+      graphiql: true,
+      rootValue,
+      schema: schema,
+    }),
+  );
+}
