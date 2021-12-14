00f7be4568f1cb18a4d1d4e7d3c3c331b2063c2d
Chris Barker <bluquar@gmail.com>
2021-12-14 22:39:24 +0000

    actually add file

--- src/client/general-purpose/viewer/reloadViewer.ts
+++ src/client/general-purpose/viewer/reloadViewer.ts
@@ -1,5 +1,5 @@
 import client from '../../aid-app/graphql/client';
 
 export default function reloadViewer(): void {
-  client.refetchQueries({ include: ['AccountStatusQuery'] });
+  client.refetchQueries({ include: ['ViewerQuery'] });
 }
