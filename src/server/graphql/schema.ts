77bd39642544e68e7183a55e4d6900e787abfc6f
Chris Barker <bluquar@gmail.com>
2021-12-18 11:28:01 -0800

    GraphQL endpoint for creating an aid request (#50)

--- src/server/graphql/schema.ts
+++ src/server/graphql/schema.ts
@@ -2,6 +2,11 @@ import type { Express } from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import { buildSchema } from 'graphql';
 import {
+  createAidRequest,
+  GQL_AID_REQUEST_TYPES,
+  GQL_CREATE_AID_REQUEST_MUTATION,
+} from '../aid_requests';
+import {
   CURRENT_USER_PAYLOAD_GQL,
   login,
   logout,
@@ -27,13 +32,17 @@ const schema = buildSchema(`
       username: String!
       password: String!
     ): CurrentUserPayload
+
+    ${GQL_CREATE_AID_REQUEST_MUTATION}
   }
 
   ${USER_GQL}
   ${CURRENT_USER_PAYLOAD_GQL}
+  ${GQL_AID_REQUEST_TYPES}
 `);
 
 const rootValue = {
+  createAidRequest,
   login,
   logout,
   me,
