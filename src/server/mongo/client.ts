accc2d83ac5ea824e8bb27ae4c0eb92bfad6a5fd
Chris Barker <bluquar@gmail.com>
2022-01-11 16:41:22 -0800

    Update client.ts

--- src/server/mongo/client.ts
+++ src/server/mongo/client.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const PASSWORD = process.env.MONGODB_USER_0_PASSWORD;
 const CLUSTER = 'aid-app-cluster-0';
-const DEFAULT_DB_NAME = 'AidApp-Test';
+const DEFAULT_DB_NAME = 'AidApp';
 const DB_USER_NAME = 'aid-app-user-0';
 
 export const MONGO_DB_URI =
