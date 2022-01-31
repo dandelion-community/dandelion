81889d41b9668a741b107c95fbd14054b581cb4c
Chris Barker <bluquar@gmail.com>
2022-01-31 08:58:42 -0800

    Support emoji names

--- src/client/components/Monogram.tsx
+++ src/client/components/Monogram.tsx
@@ -1,41 +1,76 @@
+import emojiRegexConstructor from 'emoji-regex';
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import chooseArrayElementFromHashOfString from 'src/shared/utils/chooseArrayElementFromHashOfString';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore ts thinks outline: 'none' is invalid but it's not
+import unicodeSubstring from 'unicode-substring';
+
+const emojiRegex = emojiRegexConstructor();
 
 type Props = {
   name: string;
 };
 
-const SIZE = 16;
+const DIAMETER = 16;
+const INTER_MONOGRAM_SPACING = 4;
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    borderRadius: DIAMETER / 2,
+    flexDirection: 'row',
+    height: DIAMETER,
+    marginRight: INTER_MONOGRAM_SPACING,
+    width: DIAMETER,
+  },
+  lowOpacityBackgroundCircle: {
+    opacity: 0.3,
+  },
+  text: {
+    color: 'white',
+    fontSize: DIAMETER * (10 / 16),
+    fontWeight: 'bold',
+    textAlign: 'center',
+    width: '100%',
+  },
+  textWrapperForLowOpacityBackgroundCircle: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    height: DIAMETER,
+    transform: [{ translateX: -DIAMETER - INTER_MONOGRAM_SPACING }],
+    width: DIAMETER,
+  },
+});
 
 export default function Monogram({ name }: Props): JSX.Element {
-  return (
-    <View
-      style={{
-        alignItems: 'center',
-        backgroundColor: chooseArrayElementFromHashOfString(
-          name,
-          MONOGRAM_COLORS,
-        ),
-        borderRadius: 8,
-        flexDirection: 'row',
-        height: SIZE,
-        width: SIZE,
-      }}
-    >
-      <Text
-        style={{
-          color: 'white',
-          fontSize: SIZE * (10 / 16),
-          fontWeight: 'bold',
-          textAlign: 'center',
-          width: '100%',
-        }}
-      >
-        {name.substring(0, 1)}
-      </Text>
-    </View>
+  const firstCharacter = unicodeSubstring(name, 0, 1);
+  const isEmoji = emojiRegex.test(firstCharacter);
+  const backgroundColor = chooseArrayElementFromHashOfString(
+    name,
+    MONOGRAM_COLORS,
   );
+
+  const text = <Text style={styles.text}>{firstCharacter}</Text>;
+  if (isEmoji) {
+    // Since emojis are already colorful, modify the background color
+    // to have 30% opacity
+    return (
+      <View style={styles.container}>
+        <View
+          style={[
+            styles.container,
+            styles.lowOpacityBackgroundCircle,
+            { backgroundColor },
+          ]}
+        ></View>
+        <View style={styles.textWrapperForLowOpacityBackgroundCircle}>
+          {text}
+        </View>
+      </View>
+    );
+  }
+  return <View style={[styles.container, { backgroundColor }]}>{text}</View>;
 }
 
 const MONOGRAM_COLORS: Array<string> = [
