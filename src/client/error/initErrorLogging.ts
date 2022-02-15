406ea41be8753c00319ad316eaf88131ac8ebb94
Chris Barker <bluquar@gmail.com>
2022-02-15 07:41:46 -0800

    don't log bugsnag in dev

--- src/client/error/initErrorLogging.ts
+++ src/client/error/initErrorLogging.ts
@@ -1,5 +1,6 @@
 import Bugsnag from '@bugsnag/expo';
 import BugsnagPluginReact from '@bugsnag/plugin-react';
+import Constants from 'expo-constants';
 
 let initialized = false;
 
@@ -7,7 +8,12 @@ export default function initErrorLogging(): void {
   if (!initialized) {
     Bugsnag.start({
       apiKey: '10aad92a8ec0e1998f3f466ae42fdfc9',
+      enabledReleaseStages: ['production'],
       plugins: [new BugsnagPluginReact()],
+      releaseStage:
+        Constants.manifest?.packagerOpts?.dev === true
+          ? 'development'
+          : 'production',
     });
   }
   initialized = true;
