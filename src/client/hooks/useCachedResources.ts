6371512ec2e2372b2fa7f058dbd5657bb8693df6
Chris Barker <bluquar@gmail.com>
2022-02-15 07:33:32 -0800

    restore loading of fontawesome

--- src/client/hooks/useCachedResources.ts
+++ src/client/hooks/useCachedResources.ts
@@ -1,3 +1,5 @@
+import { FontAwesome } from '@expo/vector-icons';
+import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import * as React from 'react';
 import { initializeAidRequestDrafts } from 'src/client/aid_request/drafts/AidRequestDraftsMemoryStore';
@@ -12,7 +14,12 @@ export default function useCachedResources() {
       try {
         SplashScreen.preventAutoHideAsync();
 
-        await Promise.all([initializeAidRequestDrafts()]);
+        await Promise.all([
+          Font.loadAsync({
+            ...FontAwesome.font,
+          }),
+          initializeAidRequestDrafts(),
+        ]);
       } catch (e) {
         reportError(e);
       } finally {
